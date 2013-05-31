519004aeaebec984ce24e1bd85141852f0829021
Mark Rickert <mjar81@gmail.com>
2013-05-31 08:06:39 -0400

    Add average color to UIImage.

---
+++ app/Classes/UIImage.rb
@@ -0,0 +1,27 @@
+class UIImage
+
+	def average_color
+    colorSpace = CGColorSpaceCreateDeviceRGB()
+    rgba = []
+    context = CGBitmapContextCreate(rgba, 1, 1, 8, 4, colorSpace, KCGImageAlphaPremultipliedLast | KCGBitmapByteOrder32Big)
+
+    CGContextDrawImage(context, CGRectMake(0, 0, 1, 1), self.CGImage)
+    CGColorSpaceRelease(colorSpace)
+    CGContextRelease(context)
+
+    if rgba[3] > 0)
+      alpha = rgba[3]/255.0
+      multiplier = alpha/255.0
+      UIColor.colorWithRed:(rgba[0].to_f * multiplier,
+                     green:(rgba[1].to_f * multiplier,
+                      blue:(rgba[2].to_f * multiplier,
+                     alpha:alpha)
+    else
+      UIColor.colorWithRed:(rgba[0])/255.0,
+                     green:(rgba[1])/255.0,
+                      blue:(rgba[2])/255.0,
+                     alpha:(rgba[3])/255.0)
+  	end
+	end
+
+end
