7bea6bdc9d308939dd115ac62a6cc2011d27d45e
Mark Rickert <mjar81@gmail.com>
2013-05-31 09:23:37 -0400

    New avcapture code.

--- app/Screens/SRMAnalyzerScreen.rb
+++ app/Screens/SRMAnalyzerScreen.rb
@@ -2,19 +2,46 @@ class SRMAnalyzerScreen < PM::Screen
 
   title "SRM Analyzer"
 
+  attr_accessor :vImagePreview, :stillImageOutput, :vImage
+
   def will_appear
     @view_loaded ||= begin
-      @captureManager = CaptureSessionManager.new
-      @captureManager.addVideoInput
-      @captureManager.addVideoPreviewLayer
 
-      layerRect = self.view.layer.bounds
-      @captureManager.previewLayer.setBounds(layerRect)
-      @captureManager.previewLayer.setPosition(CGPointMake(CGRectGetMidX(layerRect), CGRectGetMidY(layerRect)))
+      # UIView Setup
       view.setBackgroundColor UIColor.whiteColor
       set_nav_bar_left_button "Done", action: :close_modal, type: UIBarButtonItemStyleDone
 
-      view.layer.addSublayer(@captureManager.previewLayer)
+      # accessors setup
+      self.vImagePreview = add UIView.alloc.initWithFrame(view.frame)
+      self.stillImageOutput = AVCaptureStillImageOutput.new
+
+      # Camera View Setup
+      @session = AVCaptureSession.alloc.init
+      @session.sessionPreset = AVCaptureSessionPresetMedium
+
+      viewLayer = self.vImagePreview.layer
+
+      captureVideoPreviewLayer = AVCaptureVideoPreviewLayer.alloc.initWithSession(@session)
+
+      captureVideoPreviewLayer.frame = self.vImagePreview.bounds
+      self.vImagePreview.layer.addSublayer(captureVideoPreviewLayer)
+
+      device = AVCaptureDevice.defaultDeviceWithMediaType(AVMediaTypeVideo)
+
+      error_ptr = Pointer.new(:object)
+      input = AVCaptureDeviceInput.deviceInputWithDevice(device, error:error_ptr)
+      error = error_ptr #De-reference the pointer.
+      if !input
+        # Handle the error appropriately.
+        NSLog("ERROR: trying to open camera: %@", error)
+      else
+        @session.addInput(input)
+        self.stillImageOutput.setOutputSettings({AVVideoCodecKey: AVVideoCodecJPEG})
+
+        @session.addOutput(stillImageOutput)
+
+        @session.startRunning
+      end
 
       #Create the gradient view.
       gradient_view_width = 100
@@ -51,8 +78,43 @@ class SRMAnalyzerScreen < PM::Screen
 
       # view.addSubview(@scanningLabel)
 
-      @captureManager.captureSession.startRunning
+      # @captureManager.captureSession.startRunning
     end
+  end
+
+  def will_disappear
+    if !@session.nil? && @session.respond_to?("running") && @session.running == true
+      @session.stopRunning
+    end
+  end
+
+  def captureNow
+    videoConnection = nil
+    stillImageOutput.connections.each do |connection|
+      connection.inputPorts.each do |port|
+        if port.mediaType == AVMediaTypeVideo
+          videoConnection = connection
+          break
+        end
+      end
+      break if videoConnection
+    end
+
+    NSLog("about to request a capture from: %@", stillImageOutput)
+    stillImageOutput.captureStillImageAsynchronouslyFromConnection(videoConnection, completionHandler:lambda do |imageSampleBuffer, error|
+      exifAttachments = CMGetAttachment( imageSampleBuffer, KCGImagePropertyExifDictionary, nil)
+      if exifAttachments
+        # Do something with the attachments.
+        NSLog("attachements: %@", exifAttachments)
+      else
+        NSLog("no attachments")
+      end
+
+      imageData = AVCaptureStillImageOutput.jpegStillImageNSDataRepresentation(imageSampleBuffer)
+      image = UIImage.alloc.initWithData(imageData)
+
+      self.vImage = image
+     end)
   end
 
   def scanButtonPressed
@@ -62,6 +124,23 @@ class SRMAnalyzerScreen < PM::Screen
 
   def hideLabel(label)
     label.setHidden(true)
+  end
+
+  def should_rotate(orientation)
+    puts "Trying to determine rotation"
+    UIDeviceOrientationPortrait == orientation
+  end
+
+  def should_autorotate
+    puts "should autorotate?"
+    false
+  end
+
+  def supported_orientations
+    puts "checking supported orientations"
+    orientations = 0
+    orientations |= UIInterfaceOrientationMaskPortrait
+    orientations
   end
 
   def close_modal
