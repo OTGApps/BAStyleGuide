5591e6a9f7cd4ad8ca4a4f627e3b294f352b1138
Mark Rickert <mjar81@gmail.com>
2013-05-31 08:23:27 -0400

    Fix uiimage compile errors

--- app/Classes/UIImage.rb
+++ app/Classes/UIImage.rb
@@ -9,18 +9,18 @@ class UIImage
     CGColorSpaceRelease(colorSpace)
     CGContextRelease(context)
 
-    if rgba[3] > 0)
+    if rgba[3] > 0
       alpha = rgba[3]/255.0
       multiplier = alpha/255.0
-      UIColor.colorWithRed:(rgba[0].to_f * multiplier,
-                     green:(rgba[1].to_f * multiplier,
-                      blue:(rgba[2].to_f * multiplier,
+      UIColor.colorWithRed(rgba[0].to_f * multiplier,
+                     green:rgba[1].to_f * multiplier,
+                      blue:rgba[2].to_f * multiplier,
                      alpha:alpha)
     else
-      UIColor.colorWithRed:(rgba[0])/255.0,
-                     green:(rgba[1])/255.0,
-                      blue:(rgba[2])/255.0,
-                     alpha:(rgba[3])/255.0)
+      UIColor.colorWithRed(rgba[0] / 255.0,
+                     green:rgba[1] / 255.0,
+                      blue:rgba[2] / 255.0,
+                     alpha:rgba[3] / 255.0)
   	end
 	end
 
